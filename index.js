d7c7ca979c8c974515e9bfc19c83c1c782c7acae
Piyush Garg <pgarg2598.ca18@chitkara.edu.in>
2019-08-03 20:26:11 +0530

    Added Features

--- index.js
+++ index.js
@@ -1,76 +1,62 @@
 // Import Packages
-const express = require('express');
+const express = require("express");
 const app = express();
-const youtubedl = require('youtube-dl');
-const fs = require('fs');
-const bodyParser = require('body-parser');
-const path = require('path');
-var mime = require('mime');
+const youtubedl = require("youtube-dl");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const path = require("path");
+var mime = require("mime");
 
 // Middlewares
-app.use(express.static('./public'));
-app.use(bodyParser.urlencoded({
+app.use(express.static("./public"));
+app.use(
+  bodyParser.urlencoded({
     extended: false
-}));
+  })
+);
+
+global.vtitle = "";
 
 // Variables
 const PORT = process.env.PORT || 5000;
 
-
 // @Routes
 
-// @Route: / 
+// @Route: /
 // @desc: Home Page
 // Access: Public
-app.get('/', (req,res)=>{
-    res.render('index');
-    res.end();
+app.get("/", (req, res) => {
+  res.render("index");
+  res.end();
 });
 
-
-// @Route: /download 
+// @Route: /download
 // @desc: Download Page
 // Access: Public
-app.post('/download', (req,res)=>{
-    
-    var url = req.body.linktext;
-    var video = youtubedl(url,['--format=18'],{cwd:__dirname});
-    video.on('info',info=>{
-       var title = info._filename;
-       pipeVideo(title);
-    });
-    function pipeVideo(title){
-        video.pipe(fs.createWriteStream(title));
-        downLoadtoClient(title);
-        
-        res.redirect('/');
-        res.end();
-        
-    }
+app.post("/download", (req, res) => {
+  var url = req.body.linktext;
+  var video = youtubedl(
+    url,
+    // Optional arguments passed to youtube-dl.
+    ["--format=18"],
+    // Additional options can be given for calling `child_process.execFile()`.
+    { cwd: __dirname }
+  );
 
-    function downLoadtoClient(title){
-        
-        res.download(`./${title}`, title, function (err) {
-            if (err) {
-              // Handle error, but keep in mind the response may be partially-sent
-              // so check res.headersSent
-              console.log('Failed download: '+title);
-              console.log('Headers: '+res.headersSent);
-              //res.sendFile(`/${title}`);
-            } else {
-              // decrement a download credit, etc.
-              console.log('Successfull download: '+title);
-            }
-          })
-    }
-    
-    
+  // Will be called when the download starts.
+  video
+    .on("info", function(info) {
+      // console.log('Download started');
+      // console.log('filename: ' + info._filename);
+      vtitle = info._filename;
+      console.log("Global: ", vtitle);
+      // console.log('size: ' + info.size);
+    })
+    .pipe(fs.createWriteStream('h.mp4'));
     
 });
 
-
-
 // Listner
-app.listen(PORT, ()=>{
-    console.log(`Server Started on PORT:${PORT}...`);
+app.listen(PORT, () => {
+  console.log(`Server Started on PORT:${PORT}...`);
 });
